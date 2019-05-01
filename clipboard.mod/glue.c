2df38ef8b2995fc9a7cbf9c8efe4be745c0a1429
woollybah <woollybah@gmail.com>
2019-05-01 06:51:18 +0100

    Fixed initialising of structs. Fixes #126.

--- clipboard.mod/glue.c
+++ clipboard.mod/glue.c
@@ -23,14 +23,14 @@
 #include "brl.mod/blitz.mod/blitz.h"
 
 clipboard_c * bmx_clipboard_new_win32(int maxRetries, int retryDelay) {
-	clipboard_opts opts;
+	clipboard_opts opts = {0};
 	opts.win32.max_retries = maxRetries;
 	opts.win32.retry_delay = retryDelay;
 	return clipboard_new(&opts);
 }
 
 clipboard_c * bmx_clipboard_new_x11(int actionTimeout, int transferSize, BBString * displayName) {
-	clipboard_opts opts;
+	clipboard_opts opts = {0};
 	opts.x11.action_timeout = actionTimeout;
 	opts.x11.transfer_size = transferSize;
 	if (displayName != &bbEmptyString) {
