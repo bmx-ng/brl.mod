4ddfc87a5ea255725831e9aa3cb27f070e3ec8d0
woollybah <woollybah@gmail.com>
2019-12-09 18:00:14 +0000

    Inlined math functions.

---
+++ math.mod/doc/trunc.bmx
@@ -0,0 +1,11 @@
+Rem
+Nearest integral not greater in magnitude than x
+End Rem
+SuperStrict
+
+Framework brl.standardio
+Import brl.math
+
+Print "Trunc(+2.7) = " + Trunc(2.7)
+Print "Trunc(-2.7) = " + Trunc(-2.7)
+Print "Trunc(-0.0) = " + Trunc(-0.0)
--- math.mod/math.c
+++ math.mod/math.c
@@ -1,8 +1,53 @@
 
-#include <math.h>
+#include <bbMath.h>
 
 #define RAD_TO_DEG 57.2957795130823208767981548141052
 #define DEG_TO_RAD 0.0174532925199432957692369076848861
+
+#if __STDC_VERSION__ >= 199901L
+extern int bbIsNan( double x );
+extern int bbIsInf( double x );
+extern double bbSqr( double x );
+extern double bbSin( double x );
+extern double bbCos( double x );
+extern double bbTan( double x );
+extern double bbASin( double x );
+extern double bbACos( double x );
+extern double bbATan( double x );
+extern double bbATan2( double y,double x );
+extern double bbSinh( double x );
+extern double bbCosh( double x );
+extern double bbTanh( double x );
+extern double bbExp( double x );
+extern double bbFloor( double x );
+extern double bbLog( double x );
+extern double bbLog10( double x );
+extern double bbCeil( double x );
+extern double bbRound( double x );
+extern double bbTrunc( double x );
+
+extern int bbIsNanf( float x );
+extern int bbIsInff( float x );
+extern float bbSqrf( float x );
+extern float bbSinf( float x );
+extern float bbCosf( float x );
+extern float bbTanf( float x );
+extern float bbASinf( float x );
+extern float bbACosf( float x );
+extern float bbATanf( float x );
+extern float bbATan2f( float y,float x );
+extern float bbSinhf( float x );
+extern float bbCoshf( float x );
+extern float bbTanhf( float x );
+extern float bbExpf( float x );
+extern float bbFloorf( float x );
+extern float bbLogf( float x );
+extern float bbLog10f( float x );
+extern float bbCeilf( float x );
+extern float bbRoundf( float x );
+extern float bbTruncf( float x );
+
+#else
 
 int bbIsNan( double x ){
 	return isnan(x) ? 1 : 0;
@@ -65,37 +110,27 @@ double bbTrunc( double x ){
 	return trunc( x );
 }
 
-// fallback for pre C99 versions
-#ifndef __STDC_VERSION__
-	#define __STDC_VERSION__ 0
-#endif
+#define RAD_TO_DEGF RAD_TO_DEG
+#define DEG_TO_RADF DEG_TO_RAD
 
-#if __STDC_VERSION__ < 199901L
-	#define RAD_TO_DEGF RAD_TO_DEG
-	#define DEG_TO_RADF DEG_TO_RAD
-
-	#define sqrtf sqrt
-	#define sinf sin
-	#define cosf cos
-	#define tanf tan
-	#define asinf asin
-	#define acosf acos
-	#define atanf atan
-	#define atan2f atan2
-	#define sinhf sinh
-	#define coshf cosh
-	#define tanhf tanh
-	#define expf exp
-	#define floorf floor
-	#define logf log
-	#define log10f log10
-	#define ceilf ceil
-	#define roundf round
-	#define truncf trunc
-#else
-	#define RAD_TO_DEGF 57.2957795
-	#define DEG_TO_RADF 0.0174532
-#endif
+#define sqrtf sqrt
+#define sinf sin
+#define cosf cos
+#define tanf tan
+#define asinf asin
+#define acosf acos
+#define atanf atan
+#define atan2f atan2
+#define sinhf sinh
+#define coshf cosh
+#define tanhf tanh
+#define expf exp
+#define floorf floor
+#define logf log
+#define log10f log10
+#define ceilf ceil
+#define roundf round
+#define truncf trunc
 
 float bbSqrf( float x ){
 	return sqrtf( x );
@@ -151,3 +186,4 @@ float bbRoundf( float x ){
 float bbTruncf( float x ){
 	return truncf( x );
 }
+#endif
